947d4d6c32a33a70c099df575f6ad667d3cfcd0a
subrat <subratj611@gmail.com>
2023-04-18 23:32:06 +0530

    chakra ui add

--- src/components/ConfTrackSection/index.jsx
+++ src/components/ConfTrackSection/index.jsx
@@ -1,3 +1,11 @@
+import {
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+  Box,
+} from "@chakra-ui/react";
 import { confTrack } from "../../constants";
 import "./conftrack.scss";
 
@@ -22,7 +30,64 @@ export default function ConfTrackSection() {
             </div>
           ))}
         </div>
-        <div className="track-date">ksjs</div>
+        <div className="track-date">
+          <Accordion allowToggle>
+            <AccordionItem>
+              <h2>
+                <AccordionButton>
+                  <Box as="span" flex="1" textAlign="left">
+                    Paper Submission Due
+                  </Box>
+                  <AccordionIcon />
+                </AccordionButton>
+              </h2>
+              <AccordionPanel pb={4}>SUBMISSION CLOSED</AccordionPanel>
+            </AccordionItem>
+
+            <AccordionItem>
+              <h2>
+                <AccordionButton>
+                  <Box as="span" flex="1" textAlign="left">
+                    Notification of Acceptance
+                  </Box>
+                  <AccordionIcon />
+                </AccordionButton>
+              </h2>
+              <AccordionPanel pb={4}>15th December 2022</AccordionPanel>
+            </AccordionItem>
+
+            <AccordionItem>
+              <h2>
+                <AccordionButton>
+                  <Box as="span" flex="1" textAlign="left">
+                    Camera Ready Submission
+                  </Box>
+                  <AccordionIcon />
+                </AccordionButton>
+              </h2>
+              <AccordionPanel pb={4}>10th January 2023</AccordionPanel>
+            </AccordionItem>
+
+            <AccordionItem>
+              <h2>
+                <AccordionButton>
+                  <Box as="span" flex="1" textAlign="left">
+                    Important Links
+                  </Box>
+                  <AccordionIcon />
+                </AccordionButton>
+              </h2>
+              <AccordionPanel pb={4}>
+                <a
+                  href="https://easychair.org/conferences/?conf=otcon2022"
+                  target="_blank"
+                >
+                  Submit Paper »
+                </a>
+              </AccordionPanel>
+            </AccordionItem>
+          </Accordion>
+        </div>
       </div>
     </div>
   );
