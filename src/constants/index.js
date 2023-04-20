d87d5521504cfd86e280b2fa3c645124c3c0111a
subrat <subratj611@gmail.com>
2023-04-20 17:13:39 +0530

    constant update

--- src/constants/index.js
+++ src/constants/index.js
@@ -52,3 +52,57 @@ export const confTrack = [
     ],
   },
 ];
+
+export const RegistrationFee = {
+  thead: [
+    {
+      th: ["Early Bird (30th Nov, 2022)", "", "", "", ""],
+    },
+    {
+      th: ["Category", "Active IEEE member", "", "Non IEEE member", ""],
+    },
+    {
+      th: ["(Author)", "INR", "USD", "INR", "USD"],
+    },
+  ],
+  tableBody: [
+    {
+      data: [
+        "Full-time student/Research scholar",
+        "₹4000",
+        "$75",
+        "₹5000",
+        "$80",
+      ],
+    },
+    {
+      data: ["Academician", "₹5000", "$80", "₹6000", "$90"],
+    },
+    {
+      data: ["Industry", "₹7000", "$110", "₹8000", "$125"],
+    },
+    {
+      data: ["Regular (31st Jan, 2023)", "", "", "", ""],
+    },
+    {
+      data: [
+        "Full-time student/Research scholar",
+        "₹6000",
+        "$90",
+        "₹7000",
+        "$110",
+      ],
+    },
+    {
+      data: ["Academician", "₹7000", "$110", "₹8000", "$125"],
+    },
+    {
+      data: ["Industry", "₹9000", "$130", "₹10000", "$140"],
+    },
+  ],
+  tableFooter: [
+    {
+      data: ["Participants/Extra authors/Listener", "", "", "₹2000", ""],
+    },
+  ],
+};
