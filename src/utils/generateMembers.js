d301c4c2d7af483f8fd28f2186d331721ba84b0a
subrat <subratj611@gmail.com>
2023-05-01 13:25:45 +0530

    function that generate text to object data

---
+++ src/utils/generateMembers.js
@@ -0,0 +1,18 @@
+export function generateMembers(text) {
+  const newLineSeparatedArr = text.split("\n");
+  const internationAdvisoryBoard = [];
+
+  newLineSeparatedArr.map((data) => {
+    const boardMember = data.split(",");
+
+    const item = {
+      name: boardMember[0].trim(),
+      institute: boardMember[1].trim(),
+      place: boardMember[2].trim(),
+    };
+
+    internationAdvisoryBoard.push(item);
+  });
+
+  return internationAdvisoryBoard;
+}
