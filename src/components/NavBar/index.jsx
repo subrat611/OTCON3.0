111acc1a0b9dbad241d054274c79ff5b5f200b6b
subrat <subratj611@gmail.com>
2023-05-02 16:25:19 +0530

    refactor

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -7,6 +7,19 @@ export default function NavBar() {
   const [toggleNav, setToggleNav] = useState(false);
   const [toggleDropMenu, setToggleDropMenu] = useState(false);
 
+  const handleToggleAll = () => {
+    setToggleDropMenu((prev) => !prev);
+    setToggleNav((prev) => !prev);
+  };
+
+  const handleToggleMenu = () => {
+    setToggleDropMenu((prev) => !prev);
+  };
+
+  const handleToggleNav = () => {
+    setToggleNav((prev) => !prev);
+  };
+
   return (
     <>
       <nav className="navbar-wrapper">
@@ -25,29 +38,21 @@ export default function NavBar() {
             <Link to="/404">Call for papers</Link>
           </li>
           <li>
-            <Link onClick={() => setToggleDropMenu((prev) => !prev)}>
-              Committees
-            </Link>
+            <Link onClick={handleToggleMenu}>Committees</Link>
             {toggleDropMenu && (
               <ul className="drop-down-menu">
-                <li onClick={() => setToggleDropMenu((prev) => !prev)}>
-                  Organizing Committees
-                </li>
-                <li onClick={() => setToggleDropMenu((prev) => !prev)}>
-                  Technical Sponsor Committees
-                </li>
+                <li onClick={handleToggleMenu}>Organizing Committees</li>
+                <li onClick={handleToggleMenu}>Technical Sponsor Committees</li>
                 <li>
                   <Link
                     to="/committees/technical-program-committees"
-                    onClick={() => setToggleDropMenu((prev) => !prev)}
+                    onClick={handleToggleMenu}
                   >
                     Technical Program Committees
                   </Link>
                 </li>
-                <li onClick={() => setToggleDropMenu((prev) => !prev)}>
-                  Extended Support Committees
-                </li>
-                <li onClick={() => setToggleDropMenu((prev) => !prev)}>
+                <li onClick={handleToggleMenu}>Extended Support Committees</li>
+                <li onClick={handleToggleMenu}>
                   Conference Execution Committees
                 </li>
               </ul>
@@ -85,54 +90,48 @@ export default function NavBar() {
               toggleNav ? "navbar-mob-links visible" : "navbar-mob-links"
             }
           >
-            <li onClick={() => setToggleNav((prev) => !prev)}>
+            <li onClick={handleToggleNav}>
               <Link to="/">Home</Link>
             </li>
-            <li onClick={() => setToggleNav((prev) => !prev)}>
+            <li onClick={handleToggleNav}>
               <Link to="/conference-schedule">Conference Schedule</Link>
             </li>
-            <li onClick={() => setToggleNav((prev) => !prev)}>
+            <li onClick={handleToggleNav}>
               <Link to="/registration">Registration</Link>
             </li>
-            <li onClick={() => setToggleNav((prev) => !prev)}>
+            <li onClick={handleToggleNav}>
               <Link to="/404">Call for papers</Link>
             </li>
             <li>
-              <Link onClick={() => setToggleDropMenu((prev) => !prev)}>
-                Committees
-              </Link>
+              <Link onClick={handleToggleMenu}>Committees</Link>
               {toggleDropMenu && (
                 <ul className="drop-down-mob-menu">
-                  <li onClick={() => setToggleNav((prev) => !prev)}>
-                    Organizing Committees
-                  </li>
-                  <li onClick={() => setToggleNav((prev) => !prev)}>
+                  <li onClick={handleToggleAll}>Organizing Committees</li>
+                  <li onClick={handleToggleAll}>
                     Technical Sponsor Committees
                   </li>
                   <li>
                     <Link
                       to="/committees/technical-program-committees"
-                      onClick={() => setToggleDropMenu((prev) => !prev)}
+                      onClick={handleToggleAll}
                     >
                       Technical Program Committees
                     </Link>
                   </li>
-                  <li onClick={() => setToggleNav((prev) => !prev)}>
-                    Extended Support Committees
-                  </li>
-                  <li onClick={() => setToggleNav((prev) => !prev)}>
+                  <li onClick={handleToggleAll}>Extended Support Committees</li>
+                  <li onClick={handleToggleAll}>
                     Conference Execution Committees
                   </li>
                 </ul>
               )}
             </li>
-            <li onClick={() => setToggleNav((prev) => !prev)}>
+            <li onClick={handleToggleNav}>
               <Link to="/advisory-board">Advisory Board</Link>
             </li>
-            <li onClick={() => setToggleNav((prev) => !prev)}>
+            <li onClick={handleToggleNav}>
               <Link to="/404">Keynote speaker</Link>
             </li>
-            <li onClick={() => setToggleNav((prev) => !prev)}>
+            <li onClick={handleToggleNav}>
               <Link to="/404">Contact us</Link>
             </li>
           </ul>
