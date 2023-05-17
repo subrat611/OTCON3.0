07ac8af3449d424f28994a8e3e087851a091f10a
subrat <subratj611@gmail.com>
2023-05-17 15:30:27 +0530

    minor changes

--- src/pages/committees/TechnicalProgramCommittees.jsx
+++ src/pages/committees/TechnicalProgramCommittees.jsx
@@ -10,6 +10,23 @@ export default function TechnicalProgramCommittees() {
   return (
     <Wrapper>
       <h1 className="title">Technical Program Committees</h1>
+      <p className="subtitle">
+        The Institute of Electrical and Electronics Engineers, Incorporated
+        (IEEE) is a non-profit corporation, incorporated in the state of New
+        York on 16 March 1896. The purpose of the IEEE is to engage exclusively
+        in scientific and educational activities within the meaning of Section
+        501(c)(3) of the Internal Revenue Code of 1986, as amended, directed
+        toward the advancement of the theory and practice of electrical,
+        electronics, communications and computer engineering, as well as
+        computer science, the allied branches of engineering and the related
+        arts and sciences, which benefit the engineering profession and the
+        public. As authorized by the IEEE Board of Directors, the IEEE Member
+        and Geographic Activities Board (known as IEEE Regional Activities Board
+        prior to 1 January 2008) approved the establishment and boundaries of
+        the IEEE MP Section in 2021. The Madhya Pradesh Section conducts
+        activities which assist the local IEEE members to enhance their
+        professional careers and their communities.
+      </p>
       <div className="tpc-wrapper">
         <div className="swiper">
           {generateMembers(technicalProgramCommittees).map(
