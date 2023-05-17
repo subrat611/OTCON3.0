9596a5dbd0626f256c628896268454dbbaa4ff3c
subrat <subratj611@gmail.com>
2023-05-17 16:22:12 +0530

    link add

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,9 +4,45 @@ import ConfTrackSection from "../components/ConfTrackSection";
 import Footer from "../components/Footer";
 import HeroSection from "../components/HeroSection";
 
+import "./styles/home.scss";
+
 export default function Home() {
   return (
     <>
+      <p className="badge-previous-confot">
+        OTCON 2.0{" "}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="ionicon"
+          viewBox="0 0 512 512"
+        >
+          <path
+            d="M208 352h-64a96 96 0 010-192h64M304 160h64a96 96 0 010 192h-64M163.29 256h187.42"
+            fill="none"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="36"
+          />
+        </svg>
+      </p>
+      <p className="badge-previous-confeti">
+        ETI 2021{" "}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="ionicon"
+          viewBox="0 0 512 512"
+        >
+          <path
+            d="M208 352h-64a96 96 0 010-192h64M304 160h64a96 96 0 010 192h-64M163.29 256h187.42"
+            fill="none"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="36"
+          />
+        </svg>
+      </p>
       <HeroSection />
 
       <AboutSection>
@@ -30,6 +66,48 @@ export default function Home() {
           proceeding of the OTCON Will be submitted for inclusion to IEEE XPLORE
         </p>
       </AboutSection>
+
+      <div className="about-previos-section">
+        <AboutSection>
+          <h2 className="title">About Previous Conference</h2>
+          <div className="badge-wrapper">
+            <p className="badge-previous-conf-mob-otcon">
+              OTCON 2.0{" "}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="ionicon"
+                viewBox="0 0 512 512"
+              >
+                <path
+                  d="M208 352h-64a96 96 0 010-192h64M304 160h64a96 96 0 010 192h-64M163.29 256h187.42"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="36"
+                />
+              </svg>
+            </p>
+            <p className="badge-previous-conf-mob-eti">
+              ETI 2021{" "}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="ionicon"
+                viewBox="0 0 512 512"
+              >
+                <path
+                  d="M208 352h-64a96 96 0 010-192h64M304 160h64a96 96 0 010 192h-64M163.29 256h187.42"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="36"
+                />
+              </svg>
+            </p>
+          </div>
+        </AboutSection>
+      </div>
 
       <AboutSection>
         <h2 className="title">About OPJU</h2>
