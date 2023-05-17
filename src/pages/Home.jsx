9527380fc20e074d85023d1adbcbeb37ddc65c9e
subrat <subratj611@gmail.com>
2023-05-17 16:27:21 +0530

    minor changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,40 +9,6 @@ import "./styles/home.scss";
 export default function Home() {
   return (
     <>
-      <p className="badge-previous-confot">
-        OTCON 2.0{" "}
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="ionicon"
-          viewBox="0 0 512 512"
-        >
-          <path
-            d="M208 352h-64a96 96 0 010-192h64M304 160h64a96 96 0 010 192h-64M163.29 256h187.42"
-            fill="none"
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="36"
-          />
-        </svg>
-      </p>
-      <p className="badge-previous-confeti">
-        ETI 2021{" "}
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="ionicon"
-          viewBox="0 0 512 512"
-        >
-          <path
-            d="M208 352h-64a96 96 0 010-192h64M304 160h64a96 96 0 010 192h-64M163.29 256h187.42"
-            fill="none"
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="36"
-          />
-        </svg>
-      </p>
       <HeroSection />
 
       <AboutSection>
